7db15f45cf4ea7ce6bd2929d6d2c06458add8d4e
Rodrigo Pestana <digoonrails@gmail.com>
2011-08-09 10:10:43 -0300

    added some tests for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+  attr_accessor :password_confirmation
+  validates :login, :email, :password, :presence => true
+  # validates :login, :email, :uniqueness => true, :case_sensitive => false
+  validates_format_of       :login, :with => /^[a-zA-Z]{2}(\w+)?$/
+  validates_length_of       :login, :minimum => 2
+  
+  before_create { |u| u.admin = true if User.count == 0 }
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "first user becomes admin" do
+    User.delete_all
+    u=User.create(:email => "bob@uol.com", :password => "zoegirl", :password_confirmation => "zoegirl")
+    u.login="bobby"
+    assert u.save
+    assert u.admin?
+    u=User.create(:email => "woody@aol.com", :password => "zoegirl", :password_confirmation => "zoegirl")
+    u.login="woody"
+    assert ! u.admin?
+  end
+
+  test "no stupid logins" do
+    u = users(:sam)
+    %w(bob max123 imthemanbetyyoucanttouchme th_supaman).each do |login|
+      u.login = login
+      assert u.valid?
+    end
+    
+    ['007', '123456789', 'f451', "sam'er", 'samm-ie', "<script>alert('im in ur base, killin ur d00ds')</script."].each do |login|
+      u.login = login
+      assert ! u.valid?
+    end
+    
+  end
+
 end
