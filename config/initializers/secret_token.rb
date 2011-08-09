47eec507b5f1490b0a9fb544f3530d5efe2bf581
Rodrigo Pestana <digoonrails@gmail.com>
2011-08-08 22:07:43 -0300

    Initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Forum::Application.config.secret_token = '06086a59a4bfbdb390677b8e52a26c1e04db334d1eec14dca1be576806de6fd09bcb1f6867c8c4c1324f1b9a7acc16ddc8cdcbc404ccb684549f4d59bb5c2d94'
