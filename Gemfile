5be5c346f9707384ae90df3ad57f9ed584a572f6
Adriano Tadao <drianotadao@gmail.com>
2011-08-09 23:09:30 -0300

    Added others gems

--- Gemfile
+++ Gemfile
@@ -3,21 +3,39 @@ source 'http://rubygems.org'
 gem 'rails', '3.1.0.rc5'
 gem 'arel', '2.1.4'
 
-gem 'sqlite3'
+gem 'sqlite3', '1.3.4'
 
-gem 'haml'
-gem "haml-rails"
-gem 'rails3-generators'
+gem 'haml', '3.1.2'
+gem 'sass', '3.1.4'
+
+gem 'rails3-generators', '0.17.4'
+
+gem 'jquery-rails', '1.0.12'
+gem 'simple_form', '1.4.2'
+
+gem 'jammit', '0.6.3'
+gem 'yui-compressor', '0.9.6'
+gem 'closure-compiler', '1.1.1'
+
+gem 'settingslogic', '2.0.6'
+
+gem 'friendly_id', '3.2.1.1'
+
+group :development do
+  gem "haml-rails", '0.3.4'  
+end
 
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
   gem 'sass-rails', "~> 3.1.0.rc"
   gem 'coffee-rails', "~> 3.1.0.rc"
-  gem 'uglifier'
+  gem 'uglifier', '1.0.0'
 end
 
-gem 'jquery-rails'
+group :production do
+  gem 'exception_notification', :git => 'https://github.com/rails/exception_notification.git', :require => 'exception_notifier'
+end
 
 # Use unicorn as the web server
 # gem 'unicorn'
--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,8 @@ Bundler.require *Rails.groups(:assets) if defined?(Bundler)
 
 module Forum
   class Application < Rails::Application
+    require "#{Rails.root}/app/models/settings"
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -25,7 +27,7 @@ module Forum
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Brasilia'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -44,5 +46,10 @@ module Forum
       # g.fixture_replacement :factory_girl
       g.template_engine :haml
     end
+    
+    config.after_initialize do
+      Haml::Template.options[:format] = :xhtml      
+    end
+  
   end
 end
