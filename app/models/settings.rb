65be3788d90c7f62ad4c8300136414a0be461169
Rodrigo Pestana <digoonrails@gmail.com>
2011-08-11 22:32:08 -0300

    configured settings to use with tests

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -1,4 +1,4 @@
 class Settings < Settingslogic
-  source "#{Rails.root}/config/application.yml"
-  namespace Rails.env
+  source "#{::Rails.root}/config/application.yml"
+  namespace ::Rails.env
 end
