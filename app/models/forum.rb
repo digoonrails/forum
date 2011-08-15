f5951f9ef33056c8d9190a7ee86311d1b3c19cdf
Adriano Tadao <drianotadao@gmail.com>
2011-08-15 19:30:21 -0300

    Changes in models

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -1,2 +1,5 @@
 class Forum < ActiveRecord::Base
+  #validations
+  validates_presence_of :name
+  
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,7 @@
 class Topic < ActiveRecord::Base
   belongs_to :forum
   belongs_to :user
+  
+  validates_presence_of :title, :user, :forum
+  validates_associated :user, :forum
 end
